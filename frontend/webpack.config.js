3dd863ad7fc8b0404d7f35332d16d50f64273366
ecjanvier <ecjanvier.webdev@gmail.com>
2023-01-01 14:26:18 +0100

    Config p.22

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,5 +2,5 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: {},
+  entry: { main: path.join(__dirname, "src/index.js") },
 };
