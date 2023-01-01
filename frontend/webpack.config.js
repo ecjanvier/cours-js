9c1ba0d10339966f82a75aef9cc55093110c2ba4
ecjanvier <ecjanvier.webdev@gmail.com>
2023-01-01 14:28:26 +0100

    Config p.24

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -3,5 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: { main: path.join(__dirname, "src/index.js") },
-  output: {},
+  output: {
+    path: path.join(__dirname, "dist"),
+  },
 };
