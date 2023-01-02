9e3f41aa698c33d0b7063fbcb6494b3468358c9b
ecjanvier <ecjanvier.webdev@gmail.com>
2023-01-02 14:32:32 +0100

    Config p.35

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,15 +2,24 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: { main: path.join(__dirname, "src/index.js") },
+  entry: {
+    main: path.join(__dirname, "src/index.js"),
+  },
   output: {
     path: path.join(__dirname, "dist"),
     filename: "[name].bundle.js",
   },
   module: {
     rules: [
-      { test: /\.js/, exclude: /(node_modules)/, use: ["babel-loader"] },
-      { test: /\.css$/i, use: ["style-loader", "css-loader"] },
+      {
+        test: /\.js/,
+        exclude: /(node_modules)/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
     ],
   },
 };
