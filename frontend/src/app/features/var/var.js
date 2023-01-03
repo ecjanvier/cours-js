2039afc3b991ca66b85d06f2610b40859b720c7e
ecjanvier <ecjanvier.webdev@gmail.com>
2023-01-03 22:25:12 +0100

    Var exemple p.7

--- frontend/src/app/features/var/var.js
+++ frontend/src/app/features/var/var.js
@@ -1,7 +1,9 @@
-// Problème 1, confusion dans les blocs {} :
+// Problème n°1 : confusion dans les blocs {}
 var x = 1;
 if (true) {
-  var x = 2; // même variable, car sa portée n’est pas celle du bloc !
+  var x = 2; // même variable (sa portée n’est pas celle du bloc)
   console.log(x); // 2
 }
 console.log(x); // 2
+
+// Problème n°2 : la portée
