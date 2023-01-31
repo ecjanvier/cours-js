2c6878c03e4f22899604a49a87cd75343b0a51fe
ecjanvier <ecjanvier.webdev@gmail.com>
2023-01-31 03:39:41 +0100

    Base folder : variableVar.js (réassignation p.187)

--- frontend/src/app/features/base/variableVar.js
+++ frontend/src/app/features/base/variableVar.js
@@ -27,4 +27,4 @@ var nomDeLaVariableVar;
 nomDeLaVariableVar = 1;
 nomDeLaVariableVar = [1, 2, 3];
 nomDeLaVariableVar = "nomDeLaVariableVar";
-console.log(nomDeLaVariableVar); // 'nomDeLaVariab'
+console.log(nomDeLaVariableVar); // 'nomDeLaVariabl'
