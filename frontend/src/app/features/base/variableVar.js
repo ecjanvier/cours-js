1fcde31a292045811d05f20a3aca4697360b2d16
ecjanvier <ecjanvier.webdev@gmail.com>
2023-02-02 02:28:07 +0100

    Base folder : variableVar.js (hoisting p.59)

--- frontend/src/app/features/base/variableVar.js
+++ frontend/src/app/features/base/variableVar.js
@@ -31,4 +31,4 @@ nomDeLaVariableVar = "nomDeLaVariableVar";
 console.log(nomDeLaVariableVar); // 'nomDeLaVariableVar'
 
 // hoisting
-console.log(varHoisting); // undefined à cause du hoist
+console.log(varHoisting); // undefined à cause du hoisti
