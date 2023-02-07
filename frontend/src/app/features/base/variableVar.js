5f3824ee673b1ce4969eb230b9ec4504a4985adc
ecjanvier <ecjanvier.webdev@gmail.com>
2023-02-07 02:16:54 +0100

    Base folder : variableVar.js (no unique identifier p.139)

--- frontend/src/app/features/base/variableVar.js
+++ frontend/src/app/features/base/variableVar.js
@@ -40,4 +40,4 @@ console.log(varHoisting); // 'varHoisting'
 // pas d'identifiant unique
 var idUnique = "unique";
 var idUnique = "pasUnique";
-console.log(idUnique); // 'pasUnique' car il a écrasé la premi
+console.log(idUnique); // 'pasUnique' car il a écrasé la premiè
