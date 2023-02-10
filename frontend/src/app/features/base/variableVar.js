61f31f4afa7e680d0e0378d4118a7ed6eba275e2
ecjanvier <ecjanvier.webdev@gmail.com>
2023-02-10 14:01:10 +0100

    Base folder : variableVar.js (no block scope p.77)

--- frontend/src/app/features/base/variableVar.js
+++ frontend/src/app/features/base/variableVar.js
@@ -46,4 +46,4 @@ console.log(idUnique); // 'pasUnique' car il a écrasé la première valeur
 {
   var pasDeBloc = "pasDeBloc";
 }
-console.log(pasDeBloc); // 'pasDe'
+console.log(pasDeBloc); // 'pasDeB'
